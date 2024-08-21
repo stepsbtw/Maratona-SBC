24ffb7560a7c89fa7bfd3f7aac7c8c31f91300ff
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-08-21 12:14:11 -0300

    adicionei o coin change

--- dp.cpp
+++ dp.cpp
@@ -6,6 +6,27 @@ ios_base::sync_with_stdio(false);
 // MSC - Maior Subsequencia Crescente
 // Problema da mochila!
 
+void troco(){ 
+// qtd minima de numeros p chegar na soma
+    int n;
+    cin >> n;
+    int nums[n];
+    int objetivo;
+    cin >> objetivo;
+    vector<int> qt_nums(objetivo+1);
+
+    valor[0] = 0;
+    for (int sub = 1; sub <= objetivo; sub++) {
+        qt_nums[sub] = inf;
+        for (int num : nums) {
+            if (sub-num >= 0) {
+                valor[sub] = min(valor[sub], valor[sub-num]+1);
+            }
+        }
+    }
+    cout << qt_nums[objetivo];
+}
 
 int main(){
+
 }
