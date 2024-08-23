a380f76d74c16ee564a84b3a396bcc7d66382b01
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-08-23 16:55:13 -0300

    questao de logica

---
+++ 2023/polen.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// N <= 10^6, K <= 10^9
+
+int soma_digitos(int x){
+    int soma = 0;
+    while(x/10 > 0){
+        soma += x%10;
+        x /= 10;
+    }
+    soma += x%10;
+    return soma;
+}
+
+int coleta(int k_pos, int n_flores, vector<int>& flores){
+    for(int i = 0; i < k_pos-1; i++){
+        sort(flores.begin(),flores.end(),greater<int>());
+        flores.front() -= soma_digitos(flores.front());
+    }
+    return soma_digitos(flores.front());
+}
+
+int main(){
+    int n_flores, k_pos;
+    cin >> n_flores >> k_pos; // quero 0 based.
+    k_pos--;
+    vector<int> flores_inicial(n_flores-1);
+
+    for(int i = 0; i < n_flores; i++){
+        int x;
+        cin >> x;
+        flores_inicial[i] = x;
+    }
+
+    int resposta = coleta(k_pos,n_flores, flores_inicial);
+    cout << resposta;
+
+}
