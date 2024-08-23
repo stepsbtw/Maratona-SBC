d8edf1ae0f30637f60037d595286350efd515256
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-08-23 00:36:05 -0300

    added dijkstra and bellman ford

--- dfs-bfs.cpp
+++ dfs-bfs.cpp
@@ -2,6 +2,50 @@
 #include <vector>
 using namespace std;
 
+const int INF = 1e9;
+
+void dijkstra(int no, int n_nos,
+vector<pair<int,int>> adj
+){
+   vector<bool> foiProcessado;
+   priority_queue<int> fila;
+   for (int i = 1; i <= n; i++) {
+       dists[i] = INF;
+   }
+   dists[no] = 0;
+   q.push({0,no});
+   while (!fila.empty()) {
+      int no1 = fila.top().second; fila.pop();
+      if (foiProcessado[no1]) continue;
+      foiProcessado[no1] = true;
+      for (pair<int,int> u : adj[a]) {
+         int no2 = u.first, peso = u.second;
+         if (dists[no1]+peso < dists[no2]) {
+            dists[no2] = distance[no1]+peso;
+            fila.push({-distance[no2],no2});
+         }
+      }
+   }
+}
+
+
+void bellman_ford(int no, int n_nos,
+vector<tuple<int,int,int>> arestas
+vector<int>& dists
+){
+    for(int i = 0; i<n_nos; i++){
+        dists[i] = INF;
+    }
+    dists[no] = 0;
+    for(int i = 0; i<n_nos-1;i++){
+        for(auto a : arestas_bi){
+            int x,y,dist;
+            tie(x,y,dist) = a;
+            dists[y] = min(dists[y],dists[x]+dist);
+        }
+    }
+}   
+
 void dfs(int no, vector<vector<int>> adj, vector<bool>& visitado){
   if(visitado[no]) { return; }
   
@@ -45,5 +89,4 @@ int main(){
   // dfs precisa do "visitados" fora da funcao, pq é recursivo.
   vector<bool> visitado(n);
 
-  dfs(adj);
-  bfs(adj);
+}
