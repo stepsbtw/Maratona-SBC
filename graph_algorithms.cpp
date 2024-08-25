cb15cccbb96f329fe6dcc23b52a3d838f7c3e5c6
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-08-25 18:17:29 -0300

    Update graph_algorithms.cpp

--- graph_algorithms.cpp
+++ graph_algorithms.cpp
@@ -1,85 +1,106 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 const int INF = 1e9;
 
-//int vector<int> dijkstra(int no_inicial, int n_nos, vector<vector<pair<int, int>>> adj) {
-vector<int> dijkstra(int no_inicial, int n_nos, vector<vector<pair<int, int>>> adj) {
+// int vector<int> dijkstra(int no_inicial, int n_nos, vector<vector<pair<int, int>>> adj) {
+vector<int> dijkstra(int no_inicial, int n_nos, vector<vector<pair<int, int>>> adj)
+{
     // encontrar os menores caminhos possiveis para todo os nós ou um especifico.
     vector<bool> foiProcessado(n_nos, false);
     vector<int> dists(n_nos, INF);
-    
+
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> fila;
     // priority queue inversa!
 
     dists[no_inicial] = 0;
     fila.push({0, no_inicial});
 
-    while (!fila.empty()) {
+    while (!fila.empty())
+    {
         int no1 = fila.top().second;
         fila.pop();
 
         // if (no1 == no_destino){ return dist[no1] }; // caso queira parar no no_destino
-        if (foiProcessado[no1]) continue;
+        if (foiProcessado[no1])
+            continue;
         foiProcessado[no1] = true;
 
-        for (pair<int, int> u : adj[no1]) {
-          int no2 = u.first;
-          int peso = u.second;
-          if (dist[no1] + peso < dists[no2]) {
-              dists[no2] = dist[no1] + peso;
-              fila.push({dists[no2], no2});
-          }
+        for (pair<int, int> u : adj[no1])
+        {
+            int no2 = u.first;
+            int peso = u.second;
+            if (dists[no1] + peso < dists[no2])
+            {
+                dists[no2] = dists[no1] + peso;
+                fila.push({dists[no2], no2});
+            }
         }
-
     }
     // return -1; // caso nao tenha caminho para o no_destino
     return dists;
 }
 
-//int bellman_ford(int no_inicial, int no_destino, int n_nos, vector<tuple<int,int,int>> arestas){
-vector<int> bellman_ford(int no_inicial, int n_nos, vector<tuple<int,int,int>> arestas){
-    vector<int> dists(n_nos,INF);
+// int bellman_ford(int no_inicial, int no_destino, int n_nos, vector<tuple<int,int,int>> arestas){
+vector<int> bellman_ford(int no_inicial, int n_nos, vector<tuple<int, int, int>> arestas)
+{
+    vector<int> dists(n_nos, INF);
     dists[no_inicial] = 0;
 
-    for(int i = 0; i< n_nos-1; i++){
-        for(tuple<int,int,int> a : arestas){
-            int x,y,dist;
-            tie(x,y,dist) = a;
-            if(dists[y] < INF) { dists[y] = min(dists[y],dists[x]+dist); }
+    for (int i = 0; i < n_nos - 1; i++)
+    {
+        for (tuple<int, int, int> a : arestas)
+        {
+            int x, y, dist;
+            tie(x, y, dist) = a;
+            if (dists[y] < INF)
+            {
+                dists[y] = min(dists[y], dists[x] + dist);
+            }
         }
     }
-    //return dists[no_destino];
+    // return dists[no_destino];
     return dists;
-}   
-
-void dfs(int no, vector<vector<int>> adj, vector<bool>& visitado){
-    if(visitado[no]) { return; }
-  
-    visitado[no] = true;
-    for(int filho : adj[no]){
-        dfs(filho,adj,visitado);
-    }
-  
 }
 
-void bfs(vector<vector<int>> adj, vector<bool>& visitado){
+void dfs(int no, vector<vector<int>> adj, vector<bool> &visitado)
+{
+    if (visitado[no])
+    {
+        return;
+    }
+
+    visitado[no] = true;
+    for (int filho : adj[no])
+    {
+        dfs(filho, adj, visitado);
+    }
+}
+
+void bfs(vector<vector<int>> adj, vector<bool> &visitado)
+{
     queue<int> fila;
     // vector<int> distancia;
-  
+
     visitado[0] = true; // caso comece em outro no, pode alterar.
-    distancia[0] = 0; // nao precisa comecar pelo zero.
+    // distancia[0] = 0;   // nao precisa comecar pelo zero.
     fila.push(0);
-    
-    while(!fila.empty()){
-        int atual = q.front(); q.pop();
-        for(int filho : adj[atual]){
-            if(visitado[filho]) { continue; }
+
+    while (!fila.empty())
+    {
+        int atual = fila.front();
+        fila.pop();
+        for (int filho : adj[atual])
+        {
+            if (visitado[filho])
+            {
+                continue;
+            }
 
             visitado[filho] = true;
             fila.push(filho);
         }
     }
-  
 }
