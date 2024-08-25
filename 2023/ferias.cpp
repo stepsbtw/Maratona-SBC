d623e6775da15dfa740e408335395a9f5020677b
Caio Passos Torkst Ferreira <stepsbtw@gmail.com>
2024-08-25 17:48:40 -0300

    questao trivial

--- 2023/ferias.cpp
+++ 2023/ferias.cpp
@@ -3,32 +3,59 @@
 #include <queue>
 using namespace std;
 
-int solution(int disposicao, queue<int> cansativas, queue<int> revigorantes){
-   int qtAtividades=0;
-   while(cansativas && revigorantes || disposicao >= 0){
-      if(disposicao - cansativas.front()){
-         disposicao -= cansativas.front();
-         cansativas.pop();
-         qtAtividades++;
-      }
-      else{
-         disposicao += revigorantes.front();
-         revigorantes.pop();
-         qtAtivades++;
-      }
-   }
+// FAZER AS ATIVIDADES ATÉ EU NAO CONSEGUIR MAIS.
+
+// condicao de parada: nao consigo fazer mais nenhuma atividade cansativa e ja fiz todas revigorantes.
+
+int solution(int disposicao, queue<int> cansativas, queue<int> revigorantes, int qtC, int qtR)
+{
+    int qtAtividades = 0;
+
+    while(!revigorantes.empty())
+    {
+        if (disposicao >= cansativas.front())
+        {
+            disposicao -= cansativas.front();
+            cansativas.pop();
+            qtAtividades++;
+        }
+        else
+        {
+            disposicao += revigorantes.front();
+            revigorantes.pop();
+            qtAtividades++;
+        }
+    }
+    
+    // quando esvaziar a fila de revigorantes talvez eu seja capaz de fazer ainda.
+    if (disposicao >= cansativas.front())
+    {
+            disposicao -= cansativas.front();
+            cansativas.pop();
+            qtAtividades++;
+    }
+
+    return qtAtividades;
 }
 
-int main(){
-   int disposicao,qtC,qtR;
-   cin >> disposicao >> qtC >> qtR;
-   queue<int> cansativas(qtC);
-   queue<int> revigorantes(qtR);
-   
-   for(int i =0, i<qtC; i++){
-      cin >> cansativas[i];
-   }
-   for(int i =0; i<qtR; i++){
-      cin >> revigorantes[i];
-   }
+int main()
+{
+    int disposicao, qtC, qtR;
+    cin >> disposicao >> qtC >> qtR;
+    queue<int> cansativas;
+    queue<int> revigorantes;
+
+    for (int i = 0; i < qtC; i++)
+    {
+        int x;
+        cin >> x;
+        cansativas.push(x);
+    }
+    for (int i = 0; i < qtR; i++)
+    {
+        int x;
+        cin >> x;
+        revigorantes.push(x);
+    }
+    cout << solution(disposicao, cansativas, revigorantes, qtC, qtR);
 }
